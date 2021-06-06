490f05a17952fdb6ebeff00141ecab5bb679b1ac
Yakuto <yakuto01058910@yahoo.co.jp>
2021-06-07 00:20:19 +0900

    修正をしました6/7

--- lamp_dock/append_schema.sql
+++ lamp_dock/append_schema.sql
@@ -1,5 +1,5 @@
 CREATE TABLE history(
-    number INT(11) AUTO_INCREMENT,
+    order id INT(11) AUTO_INCREMENT,
     time DATETIME NOT NULL,
     total INT(11) DEFAULT 0,
 );
@@ -10,3 +10,13 @@ CREATE TABLE details(
     pieces int(11) NOT NULL,
     amount int(11) NOT NULL,
 );
+
+SELECT order_id,
+FROM history
+OUTER JOIN items
+on history.order_id = items.cart_id;
+
+SELECT name,
+FROM details
+OUTER JOIN items
+on details.name = items.name;
